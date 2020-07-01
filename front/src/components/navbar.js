0588d75f7a01e4b044d55682f0e507d304534466
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-07-01 10:16:08 -0400

    ready a propositional navbar

--- front/src/components/navbar.js
+++ front/src/components/navbar.js
@@ -8,13 +8,18 @@ const Navbar = props => {
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
                         <Link className="nav-link" to="#">Home <span className="sr-only">(current)</span></Link>
                     </li>
                     <li className="nav-item">
                         <Link className="nav-link" to="#">Link</Link>
+                    </li>
+                </ul>
+                <ul className="navbar-nav">
+                    <li>
+                        <Link className="nav-link" to="Register">Register</Link>
                     </li>
                     <li className="nav-item dropdown">
                         <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -26,9 +31,6 @@ const Navbar = props => {
                             <div className="dropdown-divider"></div>
                             <Link className="dropdown-item" to="#">Something else here</Link>
                         </div>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link disabled" to="#" tabindex="-1" aria-disabled="true">Disabled</Link>
                     </li>
                 </ul>
             </div>
