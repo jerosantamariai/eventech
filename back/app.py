100a0b43f47af9f468d2e59ab581a780ed100780
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-07-01 11:37:39 -0400

    fixed problems and complete others in back, ready for methods

--- back/app.py
+++ back/app.py
@@ -1,19 +1,43 @@
-from flask import Flask, jsonify, request, render_template
+import os  
+from flask import Flask, jsonify, request, render_template, send_from_directory
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-from models import db
+from models import db, Roles, Users
+from flask_mail import Mail, Message
+from werkzeug.utils import secure_filename
+from functions import allowed_file
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+ALLOWED_EXTENSIONS_IMAGES = {'png', 'jpg', 'jpeg', 'gif', 'svg'}
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.config['DEBUG '] = True
 app.config['ENV'] = 'development'
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'jerosantamariai@gmail.com' #La cuenta de correo electronico de donde saldran los correos
+app.config['MAIL_PASSWORD'] = ''
+app.config['UPLOAD_FOLDER'] = os.path.join(BASE_DIR, 'static')
+jwt = JWTManager(app)
+
+db.init_app(app)
 
 Migrate(app, db)
 CORS(app)
-
+bcrypt = Bcrypt(app)
+mail = Mail(app)
 manager = Manager(app)
 manager.add_command("db", MigrateCommand)
 
@@ -85,6 +109,65 @@ def root():
 #         db.session.delete(todos)
 #         db.session.commit()
 #         return jsonify({"msg": "Item deleted"}), 200
+
+@manager.command
+def loadroles():
+    role = Roles()
+    role.rolename = "admin"
+
+    db.session.add(role)
+    db.session.commit()
+
+    role = Roles()
+    role.rolename = "finanzas"
+
+    db.session.add(role)
+    db.session.commit()
+
+    role = Roles()  
+    role.rolename = "staff general"
+
+    db.session.add(role)
+    db.session.commit()
+
+    role = Roles()  
+    role.rolename = "alimentacion"
+
+    db.session.add(role)
+    db.session.commit()
+
+    role = Roles()
+    role.rolename = "seguridad"
+
+    db.session.add(role)
+    db.session.commit()
+
+    role = Roles()
+    role.rolename = "protagonista"
+
+    db.session.add(role)
+    db.session.commit()
+
+    role = Roles()
+    role.rolename = "cliente"
+
+    db.session.add(role)
+    db.session.commit()
+
+    print("Roles creados")
+
+@manager.command
+def loadadmin():
+    users = Users()
+    users.email = "admin@eventech.cl"
+    users.password = bcrypt.generate_password_hash("123456")        #Its obviously the first change that I make into my webpage is to change this password of my account
+    users.role_id = "1"
+
+    db.session.add(users)
+    db.session.commit()
+
+    print("Administrador Creado! Buena suerte!")
+
 
 if __name__ == '__main__':
     manager.run()
--- back/models.py
+++ back/models.py
@@ -1,2 +1,43 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
+class Roles (db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    rolename = db.Column(db.String(50), unique=True, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "rolename": self.rolename
+        }
+
+class Users (db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=True)
+    lastname = db.Column(db.String(100), nullable=True)
+    email = db.Column(db.String(100), nullable=False)
+    avatar = db.Column(db.String(100), nullable=True, default='defaultavatar.jpg')
+    phone = db.Column(db.String(12), nullable=True)
+    cordenadas = db.Column(db.String(50), nullable=True)
+    documento = db.Column(db.String(20), nullable=True)
+    createdate = db.Column(db.DateTime, default=datetime.now())
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+    role = db.relationship(Roles)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
+            "email": self.email,
+            "avatar": self.avatar,
+            "phone": self.phone,
+            "coordenadas": self.coordenadas,
+            "documento": self.documento,
+            "createdate": self.createdate,
+            "role": self.role.serialize(),
+        }
