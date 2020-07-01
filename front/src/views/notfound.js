5462397e92926d2d76eab02aa956c4bb0e279bcd
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-07-01 09:28:56 -0400

    ready instalation and the first views

---
+++ front/src/views/notfound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = props => {
+    return(
+        <div className="notfound">
+            <h1>No hay nada aqui!!!</h1>
+        </div>
+    );
+}
+
+export default NotFound;
