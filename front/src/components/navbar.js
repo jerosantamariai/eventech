421cb39fea224759de78769e804a93420c9d85a1
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-07-02 20:08:41 -0400

    navbar renderizado segun inicio de sesión

--- front/src/components/navbar.js
+++ front/src/components/navbar.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { Context } from '../store/appContext';
 
 const Navbar = props => {
+    const { store, actions } = useContext(Context);
+
     return (
-        <nav id="navbar" className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"> 
-        {
-            
-        }
+        <nav id="navbar" className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <Link className="navbar-brand" to="/">Eventech</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
+
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
@@ -22,32 +23,48 @@ const Navbar = props => {
                     <li className="nav-item">
                         <Link className="nav-link" to="/precios">Precios</Link>
                     </li>
-                    <li>
-                        <Link className="nav-link" to="/register">Regístrate</Link>
-                    </li>
-                    <li>
-                            <Link className="nav-link" to="/login">Log-in</Link>
-                    </li>
+                    {
+                        store.currentUser === null &&
+                        <>
+                            <li>
+                                <Link className="nav-link" to="/register">Regístrate</Link>
+                            </li>
+                            <li>
+                                <Link className="nav-link" to="/login">Log-in</Link>
+                            </li>
+                        </>
+                    }
 
-                    {/* <li className="nav-item dropdown">
-                        <Link className="nav-link dropdown-toggle"  id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Usuarios
-                        </Link>
-                        <div className="dropdown-menu dropdown-menu-lg-right" aria-labelledby="navbarDropdown">
-                            <Link className="dropdown-item" to="#">Acción 1</Link>
-                            <div className="dropdown-divider"></div>
-                            <Link className="dropdown-item" to="#">Cerrar sesión</Link>
-                        </div>
-                    </li> */}
+
+                    {
+                        !!store.currentUser ||
+                        store.rolename != '' ||
+                        store.rolename != null
+                        &&
+                        <>
+                            <li className="nav-item ">
+                                <Link
+                                    className="nav-link"
+                                    to="/profile"
+                                    role="button"
+                                    aria-haspopup="true"
+                                    aria-expanded="false">
+                                    Mi perfil
+                                </Link>
+                            </li>
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <Link className="dropdown-item" onClick={() => actions.logout(props.history)}>Cerrar Sesion</Link>
+                                </li>
+                            </ul>
+                        </>
+                    }
+
                 </ul>
-                <ul className="navbar-nav">
-                    <li>
-                        <Link className="nav-link" to="/register">Cerrar sesión</Link>
-                    </li>
-                </ul>
+
             </div>
-        </nav>
+        </nav >
     )
 }
 
-export default Navbar;
+export default withRouter(Navbar);
